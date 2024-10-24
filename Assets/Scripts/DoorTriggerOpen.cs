2b35f7bf4e0e01b309040cf04abb022961687360
johnklima <klima.de.sa@gmail.com>
2024-10-24 16:17:53 +0200

    added mulit-keys and sequence checking for the door

--- Assets/Scripts/DoorTriggerOpen.cs
+++ Assets/Scripts/DoorTriggerOpen.cs
@@ -7,7 +7,7 @@ public class DoorTriggerOpen : MonoBehaviour
 {
 
     public Animation doorAnimation;
-    public Transform keyNeeded;
+    public Transform [] keysNeeded;  //in no order, but a prob to require it
     public PlayerInventory invCheck;
 
 
@@ -40,13 +40,30 @@ public class DoorTriggerOpen : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        int keyCount = invCheck.inventory.Length ;
+        int keyCount = 0;
+        
+        //iterate the player's key inventory
+        for(int i = 0; i < invCheck.inventory.Length; i++)
+        {
+            if (invCheck.inventory[i] != null)
+            {
+                //we could add order of collection here, which defatco means  the "correct" keys
+
+                keyCount++;
+            }
+            
+        }
+
+        //does this also work?
+        if (invCheck.inventory.ToString() == keysNeeded.ToString())
+            Debug.Log("yup that works both count and sequence");
+
 
         if ( other.tag == "Player" )
         {
             Debug.Log("player in door trigger");
             if (isDoorOpen == false     &&
-            isE_pressed && keyCount > 0  )
+            isE_pressed && keyCount == keysNeeded.Length)
             {
                 Debug.Log("Player Open Door");
                 doorAnimation.Play();
