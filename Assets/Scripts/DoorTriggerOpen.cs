e49eaeaa8d181327bddb51a7dcecea40b075bb29
johnklima <klima.de.sa@gmail.com>
2024-10-24 16:23:46 +0200

    tweek

--- Assets/Scripts/DoorTriggerOpen.cs
+++ Assets/Scripts/DoorTriggerOpen.cs
@@ -7,7 +7,7 @@ public class DoorTriggerOpen : MonoBehaviour
 {
 
     public Animation doorAnimation;
-    public Transform [] keysNeeded;  //in no order, but a prob to require it
+    public Transform [] keysNeeded;  //in no order, or require it
     public PlayerInventory invCheck;
 
 
@@ -46,24 +46,19 @@ public class DoorTriggerOpen : MonoBehaviour
         for(int i = 0; i < invCheck.inventory.Length; i++)
         {
             if (invCheck.inventory[i] != null)
-            {
-                //we could add order of collection here, which defatco means  the "correct" keys
-
+            {                
                 keyCount++;
             }
             
         }
 
-        //does this also work?
-        if (invCheck.inventory.ToString() == keysNeeded.ToString())
-            Debug.Log("yup that works both count and sequence");
-
 
         if ( other.tag == "Player" )
         {
             Debug.Log("player in door trigger");
-            if (isDoorOpen == false     &&
-            isE_pressed && keyCount == keysNeeded.Length)
+            if (isDoorOpen == false                                  &&
+            (isE_pressed && keyCount == keysNeeded.Length            ||
+            invCheck.inventory.ToString() == keysNeeded.ToString() )  )
             {
                 Debug.Log("Player Open Door");
                 doorAnimation.Play();
