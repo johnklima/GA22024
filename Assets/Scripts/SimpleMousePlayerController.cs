e35f6bb5280eb56d319c9aa14e413663b76e46ae
johnklima <klima.de.sa@gmail.com>
2024-10-28 15:01:43 +0100

    mouse controlled first person with gravity and jump

--- Assets/Scripts/SimpleMousePlayerController.cs
+++ Assets/Scripts/SimpleMousePlayerController.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Halvor's & John's controller
+
 public class SimpleMousePlayerController : MonoBehaviour
 {
     public CharacterController controller;
 
     public float moveSpeed = 10.0f;
+    public float GRAVITY = 10.0f;
+    public float vSpeed = 0;
+    public float jumpSpeed = 8;
+        
+        
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +25,35 @@ public class SimpleMousePlayerController : MonoBehaviour
     void Update()
     {
         // code taken from youtube video from Brackeys  https://www.youtube.com/watch?v=_QajrabyTJc
-
+        // gravity from https://discussions.unity.com/t/gravity-with-character-controller/53805/3
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         // this is what allows for the strafing
-        Vector3 xPosition = transform.position + (transform.right * moveSpeed * x * Time.deltaTime);
-        Vector3 zPosition = transform.position + (transform.forward * moveSpeed * z * Time.deltaTime);
+        //Vector3 xPosition = transform.position + (transform.right * moveSpeed * x * Time.deltaTime);
+        //Vector3 zPosition = transform.position + (transform.forward * moveSpeed * z * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            vSpeed = 0; // grounded character has vSpeed = 0...
+            if (Input.GetKeyDown("space"))
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        }
+
 
 
         Vector3 move = transform.right * x  + transform.forward * z;
-        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // apply gravity acceleration to vertical speed:
+        vSpeed -= GRAVITY * Time.deltaTime;
+
+        move.y = vSpeed;
+        move.x *= moveSpeed;
+        move.z *= moveSpeed;
+
+        controller.Move(move * Time.deltaTime);
     }
 }
